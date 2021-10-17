1fe1a0133226285002318fcd4a5739abb2e3e163
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-10-17 15:35:02 +0200

    v1

---
+++ Stock_reporter.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+data = pd.read_csv(r"C:\Users\Miro\My Drive\Python_projects\Stock_Reporter\Portfolio.csv")
+data
+
+
+    
+    
+
