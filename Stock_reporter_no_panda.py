672d9f36e9a23771c41b911c8b741b8cc4003cc2
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-11-20 16:30:43 +0100

    v01

--- Stock_reporter_no_panda.py
+++ Stock_reporter_no_panda.py
@@ -2,7 +2,7 @@ import os
 import yfinance as yf 
 import csv
 
-output = []
+output = [] #contains only ticker and purchase price
 
 #create new output just with desired rows
 with open("Portfolio.csv", "r+") as stocks: 
@@ -11,11 +11,23 @@ with open("Portfolio.csv", "r+") as stocks:
         output.append(( cells[ 0 ], cells[ 10]))
 
 
-ticker = []
+ticker = [] #contains only tickers
 
+#append ticker with tickers only
 for item in output:
     ticker.append(item[0])
 
+def current_stock_price():
+    #loop over a list of stocks and return current price of each stock
+    i = 0
+    
+    while i < len(ticker):
+        price = print(str(ticker[i]) + " " + str(yf.Ticker(ticker[i]).info['regularMarketPrice']))
+        i += 1
+    return i, price
+
+current_prices = current_stock_price()
+print(current_prices)
 
 
 
