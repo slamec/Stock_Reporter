2d619811875a9c0387f4433ba2c0a92ff8d534f0
Miroslav Kopecky <kopecky.mir@gmail.com>
2021-12-17 16:53:39 +0100

    mean added

--- Stock_reporter_panda_math - mean.py
+++ Stock_reporter_panda_math - mean.py
@@ -3,12 +3,12 @@ import pandas as pd
 #from subprocess import call
 #call(["python", "Stock_reporter_no_panda.py"])
 
-portfolio = pd.read_csv("portfolio_new.csv")
+portfolio_read = pd.read_csv("portfolio_new.csv")
 #portfolio.set_index(['Symbol', 'Purchase price', 'Current price']).groupby(level=['Symbol', 'Purchase price', 'Current price']).mean()
-df = portfolio.groupby(['Symbol'], as_index= False).mean()
-print(df)
+portfolio = portfolio_read.groupby(['Symbol'], as_index= False).mean()
+print(portfolio)
 
-divide_columns = df['Current price'] / df['Purchase price'] - 1
+divide_columns = portfolio['Current price'] / portfolio['Purchase price'] - 1
 
 percent_list = []
 for i in divide_columns:
@@ -19,8 +19,8 @@ for i in divide_columns:
 
 print(percent_list)
 
-df["Percent"] = percent_list
-df.to_csv("Stock_list_final.csv", index = False)
+portfolio["Percent"] = percent_list
+portfolio.to_csv("Stock_list_final.csv", index = False)
 
-df.to_excel ("Stock_final.xlsx", index = None, header=True)
+portfolio.to_excel ("Stock_final.xlsx", index = None, header=True)
 
